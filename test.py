1e25967290a9b4c5e0cadd3bdd2d05852b012f78
Marco Secci <marcosecci2002@gmail.com>
2023-10-21 11:30:06 +0200

    fixed bug in test.py - needs to be implemented somewhere somehow

--- test.py
+++ test.py
@@ -6,23 +6,14 @@ from vacuum_environment import room
 
 
 def create_unique_keys_matrix_dict(matrix):
-    # Define all possible values in the matrix:
-    # Create a dictionary with unique keys for each value:
-    key_mapping = {
-        value: "".join(random.choices(string.ascii_uppercase, k=5))
-        for value in range(matrix.shape[0] * matrix.shape[1])
-    }
-
     # Create an empty dictionary to to store the matrix with unique keys:
     matrix_dict = {}
 
     # Iterating through the matrix and assign unique keys:
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
-            value = matrix[i, j]  # Questo è uno tra Clean Dirty e Obstacle
-            key = key_mapping[
-                value  # Questo dovrebbe essere un numero da 0 a 15 e mai lo stesso
-            ]
+            value = matrix[i, j]  # Questo è uno tra 'Clean' 'Dirty' e 'Obstacle'
+            key = "".join(random.choices(string.ascii_uppercase, k=5))
             matrix_dict.__setitem__(key, value)
 
     return matrix_dict
